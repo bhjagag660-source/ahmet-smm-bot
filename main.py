8040338a89f2f78727f2188a39550445e8862e3b
bhjagag660-source <bhjagag660@gmail.com>
2026-03-07 21:25:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 from datetime import datetime
 import time
 # === AYARLAR ===
-TOKEN = "8675041032:AAGbddhOBWJMR8VzNDbPwfJi3LpLhifTUs4"
+TOKEN = "8675041032:AAGVGvskkp49Eok1XDyTGs96qMtWAhHd6os"
 BOT_USERNAME = "Ahmet_smmpanel_bot"
 
 # Adminler
